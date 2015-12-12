f90aaea01913a37918cf47f896fbb4c44237c703
Sakshi Udeshi <sakshiudeshi@macbook-pro.local>
2015-12-12 16:18:38 +0530

    Initial Commit

---
+++ PlacementPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlacementPractice
+//
+//  Created by Sakshi Udeshi on 12/12/15.
+//  Copyright © 2015 Sakshi Udeshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
